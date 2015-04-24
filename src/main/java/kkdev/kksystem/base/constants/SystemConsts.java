847917b25d162711e6f848774858232ec944061c
Garikk <supergarikk@gmail.com>
2015-04-24 20:16:01 +0300

    Continue migration to maven

--- src/main/java/kkdev/kksystem/base/constants/SystemConsts.java
+++ src/main/java/kkdev/kksystem/base/constants/SystemConsts.java
@@ -10,10 +10,10 @@ package kkdev.kksystem.base.constants;
  * @author blinov_is
  */
 public final class SystemConsts {
-    public static String KK_BASE_CONFPATH="../etc";
-    public static String KK_BASE_CONFPATH_CONNECTIONS="../etc/pluginconnections";
-    public static String KK_BASE_PLUGINPATH="../plugins";
-    public static String KK_BASE_EXCHANGEPATH="../exchange";
+    public static String KK_BASE_CONFPATH="./etc";
+    public static String KK_BASE_CONFPATH_CONNECTIONS="./etc/pluginconnections";
+    public static String KK_BASE_PLUGINPATH="./plugins";
+    public static String KK_BASE_EXCHANGEPATH="./exchange";
     
     public static String KK_BASE_SETTINGS_FILE="kksystem.xml.conf";
     
