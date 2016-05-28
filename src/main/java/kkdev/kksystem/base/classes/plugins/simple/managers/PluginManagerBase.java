2835d8669992515907addef8da923d6d623e9a83
Garikk <supergarikk@gmail.com>
2016-05-28 21:11:22 +0300

    Notify support

--- src/main/java/kkdev/kksystem/base/classes/plugins/simple/managers/PluginManagerBase.java
+++ src/main/java/kkdev/kksystem/base/classes/plugins/simple/managers/PluginManagerBase.java
@@ -9,7 +9,10 @@ import static java.lang.System.out;
 import java.util.HashMap;
 import java.util.Map;
 import kkdev.kksystem.base.classes.display.PinLedCommand;
+import kkdev.kksystem.base.classes.notify.NotifyConsts;
+import kkdev.kksystem.base.classes.notify.PinNotifyData;
 import kkdev.kksystem.base.classes.plugins.PluginMessage;
+import kkdev.kksystem.base.constants.PluginConsts;
 import kkdev.kksystem.base.interfaces.IPluginBaseInterface;
 import kkdev.kksystem.base.interfaces.IPluginKKConnector;
 
@@ -38,7 +41,7 @@ public  class PluginManagerBase {
         
 
     }
-        public synchronized void _BASE_SendPluginMessageDirect(String FeatureID,String PluginUUID,String PinName, Object PinData) {
+    public synchronized void _BASE_SendPluginMessageDirect(String FeatureID,String PluginUUID,String PinName, Object PinData) {
         PluginMessage Msg = new PluginMessage();
         Msg.PinName = PinName;
         Msg.PinData = PinData;
@@ -48,4 +51,20 @@ public  class PluginManagerBase {
 
     }
  
+    public synchronized void NOTIFY_SendNotifyMessage(String FeatureID,NotifyConsts.NOTIFY_TYPE NotifyType, NotifyConsts.NOTIFY_METHOD[] NotifyMethod, String NotifyText) {
+        PinNotifyData PD;
+        PD=new PinNotifyData();
+        PD.NotifyText=NotifyText;
+        PD.NotifyMethod=NotifyMethod;
+        PD.NotifyType=NotifyType;
+        
+        PluginMessage Msg = new PluginMessage();
+        Msg.PinName = PluginConsts.KK_PLUGIN_BASE_NOTIFY_DATA;
+        Msg.PinData = PD;
+        Msg.FeatureID=FeatureID;
+
+        Connector.SendPinMessage(Msg);
+        
+
+    }
 }
