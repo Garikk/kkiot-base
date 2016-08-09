7ba5b13814ee10102a45ac1fdc01753c0ba8cf8c
Garikk <supergarikk@gmail.com>
2016-08-09 20:09:16 +0300

    Prepare for quickparameters integration

--- src/main/java/kkdev/kksystem/base/classes/plugins/simple/KKPluginBase.java
+++ src/main/java/kkdev/kksystem/base/classes/plugins/simple/KKPluginBase.java
@@ -61,7 +61,7 @@ public abstract class KKPluginBase implements IPluginKKConnector{
 
     @Override
     public PluginConfiguration getQuickParameters() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+       return null;
     }
 
     @Override
