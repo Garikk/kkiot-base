47e7935d5c9213a29f7588468878b9444e7e8a22
Garikk <supergarikk@gmail.com>
2017-03-12 20:29:27 +0300

    Reorganize updater

---
+++ src/main/java/kkdev/kksystem/base/classes/plugins/weblink/WM_Answer_Configuration_Info_Pack.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package kkdev.kksystem.base.classes.plugins.weblink;
+
+/**
+ *
+ * @author sayma_000
+ */
+public class WM_Answer_Configuration_Info_Pack extends WM_Answer_Data {
+    public WM_Answer_Configuration_Info[] Pack; 
+ }
