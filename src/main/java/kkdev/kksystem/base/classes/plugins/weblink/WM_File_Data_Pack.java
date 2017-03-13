8c32b5fa65b6ddfccbae9ef5280517ccd6c6a60a
Garikk <supergarikk@gmail.com>
2017-03-13 20:31:50 +0300

    Fix Weblink

---
+++ src/main/java/kkdev/kksystem/base/classes/plugins/weblink/WM_File_Data_Pack.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package kkdev.kksystem.base.classes.plugins.weblink;
+
+/**
+ *
+ * @author blinov_is
+ */
+public class WM_File_Data_Pack  extends WM_Answer_Data {
+    public WM_File_Data[] Pack;
+}
